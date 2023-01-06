799cd9d5a2cf4dab5de0f56571025380a6a5b488
Steven Ried <70533094+stevenried@users.noreply.github.com>
2023-01-06 11:45:29 -0500

    Remove code from ExpenseItem.js to make it state-less

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 import './ExpenseItem.css'
@@ -9,21 +8,13 @@ function ExpenseItem(props) {
     currency: 'USD',
   })
 
-  const [title, setTitle] = useState(props.title)
-
-  const clickHandler = () => {
-    setTitle('Updated!!!')
-    console.log(title)
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
